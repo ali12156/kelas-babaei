76dc2ccf93baaf1ed5cb791250061290598a4dfb
MHFaramarzi <faramarzimohammad810@gmail.com>
2023-08-22 13:08:49 +0330

    delete havij

--- myapp/views.py
+++ myapp/views.py
@@ -2,7 +2,4 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import datetime as t
-@api_view(["GET"])
-def hello_havij(request):
-    return(Response({"dalam":str(t.datetime.now())}))
-# Create your views here.
+a=13
